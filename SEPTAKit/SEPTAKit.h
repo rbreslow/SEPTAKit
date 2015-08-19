dc2d64537b3f96d8963e933abaad8ed5587cea6d
c1yd3i <breslowrocky@gmail.com>
2015-08-19 15:48:02 -0400

    Initial Commit

---
+++ SEPTAKit/SEPTAKit.h
@@ -0,0 +1,19 @@
+//
+//  SEPTAKit.h
+//  SEPTAKit
+//
+//  Created by Rocky Breslow on 8/19/15.
+//  Copyright (c) 2015 Rocky Breslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SEPTAKit.
+FOUNDATION_EXPORT double SEPTAKitVersionNumber;
+
+//! Project version string for SEPTAKit.
+FOUNDATION_EXPORT const unsigned char SEPTAKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SEPTAKit/PublicHeader.h>
+
+
